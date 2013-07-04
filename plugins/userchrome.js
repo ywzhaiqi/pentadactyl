96e2b6f0b541c9110d5de9c7b7a22177c03a2362
ywzhaiqi <ywzhaiqi@gmail.com>
2013-07-05 01:26:52 +0800

    精简代码

--- plugins/userchrome.js
+++ plugins/userchrome.js
@@ -1,19 +1,12 @@
 (function(){
 
-function flushCache(file) {
-    if (file)
-         Services.obs.notifyObservers(file, "flush-cache-entry", "");
-    else
-         Services.obs.notifyObservers(null, "startupcache-invalidate", "");
-}
-
 group.commands.add(['reloaduc', 'reuc'], 'reload userChrome scripts',
     function(args){
         let name = args[0];
         let scripts = [s for each(s in userChrome_js.scripts) if(s.filename == name)];
         if(scripts.length == 0) return;
 
-        flushCache();
+        util.flushCache();
         Services.scriptloader.loadSubScript(scripts[0].url, {}, scripts[0].charset || "utf-8");
         // dactyl.execute(":source " + scripts[0].url);
     },
