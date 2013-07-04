63c4f5bd21f8a64a60c69cecf876d7f1b49e6d1c
ywzhaiqi <ywzhaiqi@gmail.com>
2013-07-05 03:30:32 +0800

    增加 uce[dit] 命令，修改 reloaduc 为 ucr[ehash]

--- plugins/userchrome.js
+++ plugins/userchrome.js
@@ -1,21 +1,46 @@
 (function(){
 
-group.commands.add(['reloaduc', 'reuc'], 'reload userChrome scripts',
-    function(args){
-        let name = args[0];
-        let scripts = [s for each(s in userChrome_js.scripts) if(s.filename == name)];
-        if(scripts.length == 0) return;
+dactyl.assert(typeof(userChrome_js) != 'undefined', "userChromeJS must exist");
 
-        util.flushCache();
-        Services.scriptloader.loadSubScript(scripts[0].url, {}, scripts[0].charset || "utf-8");
-        // dactyl.execute(":source " + scripts[0].url);
+var userChromeJS = {
+    list: function(){
+        return userChrome_js.scripts;
+    },
+    get: function(name){
+        if(!name) return;
+        return [s for each(s in userChromeJS.list()) if(s.filename == name)];
+    },
+    completer: function(context, args){
+        context.title = ["script", "description"];
+        context.completions = [[s.filename, s.description] for each(s in userChromeJS.list())];
+    }
+};
+
+group.commands.add(['uce[dit]'], 'Edit an userChrome script',
+    function(args){
+        let scripts = userChromeJS.get(args[0]);
+        if(!scripts) return;
+
+        scripts[0].file.launch();
     },
     {
         literal: 1,
-        completer: function(context, args){
-            context.title = ["script", "description"];
-            context.completions = [[s.filename, s.description] for each(s in userChrome_js.scripts)];
-        }
+        completer: userChromeJS.completer
+    },
+    true
+);
+
+group.commands.add(['ucr[ehash]'], 'Reload an userChrome script',
+    function(args){
+        let scripts = userChromeJS.get(args[0]);
+        if(!scripts) return;
+
+        util.flushCache();
+        Services.scriptloader.loadSubScript(scripts[0].url, {}, scripts[0].charset || "utf-8");
+    },
+    {
+        literal: 1,
+        completer: userChromeJS.completer
     },
     true
 );
