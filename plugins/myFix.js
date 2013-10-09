daf0a55c972a2c6fa229725ffaf529e837c70f40
ywzhaiqi <ywzhaiqi@gmail.com>
2013-10-09 14:49:34 +0800

    myFix.js 改用内置 DOM 方法

--- plugins/myFix.js
+++ plugins/myFix.js
@@ -1,6 +1,8 @@
 
-// 隐藏 zoomlevel 文字
-if($("nosquint-status"))
-    $("dactyl-statusline-field-zoomlevel").setAttribute("hidden", true)
+var $doc = DOM(document);
 
-function $(id) document.getElementById(id)
+// 隐藏 zoomlevel 文字
+if($doc.find("#nosquint-status").length)
+    $doc.find("#dactyl-statusline-field-zoomlevel").attr("hidden", true);
+
+$doc.find("#key_inspectPage").remove();  // 删除 DOM Inspector 的快捷键，跟自带的查看器冲突
