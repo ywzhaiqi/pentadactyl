5ac279ad8c9f8da8f7947cd7642525cf3278e413
ywzhaiqi <ywzhaiqi@gmail.com>
2014-06-04 02:12:08 +0800

    对 ff29 进行修正

--- plugins/myFix.js
+++ plugins/myFix.js
@@ -1,8 +1,32 @@
+/*
+  自用的对 firefox 的一些修正
+ */
 
+// DOM 为 pentadactyl 内置的仿 jQuery 库
 var $doc = DOM(document);
 
-// 隐藏 zoomlevel 文字
-if($doc.find("#nosquint-status").length)
-    $doc.find("#dactyl-statusline-field-zoomlevel").attr("hidden", true);
+// 如果用了 nosquint 扩展则隐藏 pentadactyl 自带的缩放文字，防止重复
+if($("nosquint-status")) {
+	$doc.find("#dactyl-statusline-field-zoomlevel").attr("hidden", true);
+}
 
-$doc.find("#key_inspectPage").remove();  // 删除 DOM Inspector 的快捷键，跟自带的查看器冲突
+// 删除 DOM Inspector 的快捷键，跟自带的查看器冲突
+$doc.find("#key_inspectPage").remove();
+
+
+// for firefox 29+，移动原 status-bar 图标到 dactyl-status-bar
+setTimeout(function(){
+	var dactylStatusBar = $('dactyl-status-bar');
+	if (!dactylStatusBar) {
+		// setTimeout(argument.callee, 200);
+		alert('dactyl-status-bar not exists.')
+		return;
+	}
+
+	var children = $('status-bar').children;
+	for (var i = 0, l = children.length; i < l; i++) {
+		dactylStatusBar.appendChild(children[i]);
+	}
+}, 500);
+
+function $(id) document.getElementById(id)
